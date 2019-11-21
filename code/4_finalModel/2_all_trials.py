1d4b52cd2a7b7bb850285752f4bb7e4c12d1d6eb
unknown <Greeshma Karanth>
2019-11-21 12:08:16 +0530

    Final Models

--- code/4_finalModel/2_all_trials.py
+++ code/4_finalModel/2_all_trials.py
@@ -23,50 +23,63 @@ from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv('C:/Users/grees/OneDrive/Documents/Computer Science/Data Analytics/Audio-Emotion-Recognition/extractedData/details.csv')
 
-X = df[list(df.columns)[3:]]
-X = preprocessing.normalize(X)
+X = df[list(df.columns)[3:]] #to get the relevant columnns (tonnetz, MFCC, chroma etc.)
+X = preprocessing.normalize(X) #to normalize the data
 y = df['emotion']
 X = np.asarray(X)
 y = np.asarray(y)
 X.shape, y.shape
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+#to split data into training and testing => 75:25 ratio
+
+
+#decision tree classifier
 dtree = DecisionTreeClassifier()
 dtree.fit(X_train, y_train)
-#predictions = dtree.predict(X_test)
+predictions = dtree.predict(X_test)
+accuracy=metrics.accuracy_score(y_test, predictions)
+print(classification_report(y_test,predictions))
+print("Confusion matrix for decision trees:")
+print(confusion_matrix(y_test,predictions))
+scores1 = cross_val_score(dtree, X, y, cv=10)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores1.mean(), scores1.std()))
 
+#random forest classifier
 rfor = RandomForestClassifier(n_estimators=10)
-rfor = rfor.fit(X, y)
+rfor = rfor.fit(X_train, y_train)
 predictions2 = rfor.predict(X_test)
-
+print(classification_report(y_test,predictions2))
 accuracy=metrics.accuracy_score(y_test, predictions2)
-#accuracy
+print("Confusion matrix for random forest:")
+print(confusion_matrix(y_test,predictions2))
+scores2 = cross_val_score(rfor, X, y, cv=5)      
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores2.mean(), scores2.std()))
 
-clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2, random_state=0)
-scores1 = cross_val_score(clf, X, y, cv=10)
-print(scores1.mean())                               
 
-clf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
-scores2 = cross_val_score(clf, X, y, cv=5)
-print(scores2.mean())                               
-
+#Extra trees classifier
 clf = ExtraTreesClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
 scores3 = cross_val_score(clf, X, y, cv=10)
-print(scores3.mean())
-#scores = cross_val_score(rfor, X, y, cv=10)
-#scores  
+print("\nAccuracy for Extra Trees: %0.2f (+/- %0.2f)" % (scores3.mean(), scores3.std()))
 
-clf = AdaBoostClassifier(n_estimators=100)
+#AdaBoost Classifier
+clf = AdaBoostClassifier(n_estimators=20)
 scores4 = cross_val_score(clf, X, y, cv=10)
-print(scores4.mean())
+print("\nAccuracy for AdaBoost: %0.2f (+/- %0.2f)" % (scores4.mean(), scores4.std()))
 
+#Gradient boosting classifier
 gb_clf = GradientBoostingClassifier(n_estimators=20, learning_rate=0.05, max_features=2, max_depth=2, random_state=0)
-#gb_clf.fit(X_train, y_train)
+gb = gb_clf.fit(X_train, y_train)
+predictions3 = gb.predict(X_test)
+print(classification_report(y_test,predictions3))
+accuracy=metrics.accuracy_score(y_test, predictions3)
+print("Confusion matrix for gradient boosting:")
+print(confusion_matrix(y_test,predictions3))
 scores5 = cross_val_score(gb_clf, X, y, cv=10)
-print(scores5.mean())
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores5.mean(), scores5.std()))
 
-bagging = BaggingClassifier(DecisionTreeClassifier(), max_samples=0.3, max_features=0.7)
+#Bagging using Decision Trees
+bagging = BaggingClassifier(DecisionTreeClassifier(), max_samples=0.3, max_features=0.7) #more dependent on features than samples
 scores6 = cross_val_score(bagging, X, y, cv=10)
-print(scores6.mean())
-
+print("\nAccuracy after bagging with Decision Trees: %0.2f (+/- %0.2f)" % (scores6.mean(), scores6.std()))
 
