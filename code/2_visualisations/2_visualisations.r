369ab4ac4b1d70ffa657be76f9ab9cdbe3877226
Arya Rajiv Chaloli <aryarajivchaloli@gmail.com>
2019-11-21 22:32:30 +0530

    modified visualisations

--- code/2_visualisations/2_visualisations.r
+++ code/2_visualisations/2_visualisations.r
@@ -36,41 +36,41 @@ corrplot(corAudio, method="number", main="Correlation of Housing Data")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$gender, y=audioFeatureData1$chr_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Gender by the chr_mn", y = "chr_mn", x = "Gender")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$gender, y=audioFeatureData1$ton_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Gender by the ton_mn", y = "ton_mn", x = "Gender")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$gender, y=audioFeatureData1$cont_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Gender by the cont_mn", y = "cont_mn", x = "Gender")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$chr_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the chr_mn", y = "chr_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$ton_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the ton_mn", y = "ton_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$cont_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the cont_mn", y = "cont_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$mel02_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the mel02_mn", y = "mel02_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$mel24_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the mel24_mn", y = "mel24_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$mel46_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the mel46_mn", y = "mel46_mn", x = "Emotion")
 
 ggplot(audioFeatureData1, aes(x=audioFeatureData1$emotion, y=audioFeatureData1$mel68_mn)) + 
   geom_jitter(position=position_jitter(0.2), shape = 17, col = "orange") + 
-  labs(title = "Plot of the Sale Price by the Overall Quality", x = "Overall Quality", y = "Sale Price")
+  labs(title = "Plot of the Emotion by the mel68_mn", y = "mel68_mn", x = "Emotion")
 
